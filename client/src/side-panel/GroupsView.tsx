90b03c80675bcba064050191ab5fc4cd106bad09
Yosuke Otsuka <yosuke.otsuka@develhack.com>
2026-07-07 21:45:27 +0000

    Add filter input to group list view

--- client/src/side-panel/GroupsView.tsx
+++ client/src/side-panel/GroupsView.tsx
@@ -1,6 +1,4 @@
-import { useMemo, useState } from "react";
-import type { Group } from "../types";
-import { toLocalDateTimeString } from "../logic";
+import { Input } from "@/components/ui/input";
 import {
   Select,
   SelectContent,
@@ -9,7 +7,10 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { useMemo, useState } from "react";
 import { Link, useSearchParams } from "react-router";
+import { toLocalDateTimeString } from "../logic";
+import type { Group } from "../types";
 
 type Order = "new" | "old" | "asc" | "desc";
 
@@ -33,16 +34,25 @@ interface Props {
 export default function GroupsView({ groupList }: Props) {
   const [searchParams] = useSearchParams();
 
+  const [filter, setFilter] = useState<string>("");
   const [order, setOrder] = useState<string>("new");
 
-  const sortedGroupList = useMemo(
-    () => groupList.toSorted(comparator(order as Order)),
-    [groupList, order],
+  const processedGroupList = useMemo(
+    () => groupList.filter((group) => group.name.includes(filter)).sort(comparator(order as Order)),
+    [filter, groupList, order],
   );
 
   return (
     <div className="h-full flex flex-col">
-      <div className="flex flex-row">
+      <div className="flex flex-row gap-2">
+        <Input
+          id="filter"
+          name="filter"
+          type="search"
+          placeholder="Filter"
+          onChange={(e) => setFilter(e.target.value)}
+          autoFocus
+        />
         <Select value={order} onValueChange={setOrder}>
           <SelectTrigger>
             <SelectValue />
@@ -60,7 +70,7 @@ export default function GroupsView({ groupList }: Props) {
       <div className="relative grow">
         <div className="absolute h-full w-full overflow-y-auto">
           <ul className="m-2 flex flex-col gap-2">
-            {sortedGroupList.map((group) => (
+            {processedGroupList.map((group) => (
               <li key={group.id}>
                 <Link to={`/${group.id}/*?${searchParams.toString()}`} className="flex flex-row">
                   <span className="grow truncate">{group.name}</span>
